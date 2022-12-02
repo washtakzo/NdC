e05bd1eeb74fc1177117b4e7330177f3b3c1e380
washtakzo <washtakzo@gmail.com>
2022-12-02 16:27:36 +0100

    section 2 ok

--- components/LargeButton.tsx
+++ components/LargeButton.tsx
@@ -8,7 +8,7 @@ type Props = {
 const LargeButton = ({ className, children }: Props) => {
   return (
     <button
-      className={`w-[90%] h-16 mx-auto block border-[1px] border-solid border-[#00000080] text-third hover:bg-third25 hover:text-black transition-all duration-300 ease-in-out ${className}`}
+      className={`w-[90%] h-16 mx-auto block border-[1px] border-solid border-[#00000080] hover:bg-third25 hover:text-black transition-all duration-300 ease-in-out ${className}`}
     >
       {children}
     </button>
--- pages/index.tsx
+++ pages/index.tsx
@@ -36,40 +36,40 @@ export default function Home() {
       </Head>
 
       <AnimatedBackground />
-      <main className="pt-52">
+      <main className="pt-40 sm:pt-52">
         <Header />
-        {/* TEST DIV */}
-        <section className="relative max-w-[2000px] mx-auto">
+        <section className="relative max-w-[2000px] mx-auto h-[80vh]">
           <div className="flex justify-end px-4">
             <img
-              className="aspect-[9/16] object-cover min-h-[340px] h-[28vw]  max-h-[600px]"
+              className="aspect-[9/16] object-cover min-h-[220px] h-[28vw]  max-h-[600px]"
               src="https://assets.website-files.com/601835c0c88eb62d2fa657a1/6019880417e04655dd17e50b_leisara-imZnpDXSNVA-unsplash.jpg"
               alt=""
             />
           </div>
           <Paralax
-            className="mt-40 relative md:mt-0 lg:-mt-20 xl:-mt-96"
+            className="absolute -z-10 top-[30%] md:top-[20%]"
             scrollY={scrollY}
             paralaxSpeed={40}
             scrollStartingPoint={1}
           >
             <img
-              className="aspect-[12/16] object-cover min-h-[520px] max-h-[1600px] h-[54vw]"
+              className="aspect-[12/16] object-cover min-h-[280px] max-h-[1600px] h-[54vw]"
               src="https://assets.website-files.com/60153bc9d8840978bfd8963b/60171b4ee5da29b83ff7bf6e_kevin-laminto-Wac13Oopo34-unsplash.jpg"
               alt=""
             />
           </Paralax>
-          <h2 className="top-[44%] lg:top-[25%] xl:top-[5%] left-[25%] absolute text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl max-w-[600px] font-serif">
+          <h2 className="top-[24%] lg:top-[25%] xl:top-[5%] left-[25%] absolute text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl max-w-[900px] font-serif">
             Framy Is A Fashion Lifestyle Template
           </h2>
-          <Paralax
-            className="relative"
-            scrollY={scrollY}
-            paralaxSpeed={1500}
-            scrollStartingPoint={1}
-          >
-            <LargeButton>SHOP COLLECTIONS</LargeButton>
-          </Paralax>
+          <LargeButton className="absolute left-0 right-0 bottom-[10%]">
+            SHOP COLLECTIONS
+          </LargeButton>
+        </section>
+        <section className="relative max-w-[2000px] mx-auto h-[50vh] lg:h-screen flex justify-center items-center ">
+          <h2 className="font-serif px-4 text-4xl md:text-6xl lg:text-7xl xl:text-8xl text-center max-w-[800px] mx-auto  leading-[70px]  sm:leading-[80px] md:leading-[90px] lg:leading-[100px] xl:leading-[120px]">
+            With Framy You Can Sell Clothes, Shoes, Accessories, Jewelry, You
+            Name It!
+          </h2>
         </section>
         <div className="h-[200vh] w-8">
           <div className="w-80 h-80 bg-red-600 my-12">test</div>
