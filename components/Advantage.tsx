0b470c47f6383da7047d50cdc449a82e147bef13
washtakzo <washtakzo@gmail.com>
2022-12-03 09:52:00 +0100

    v1 ok

---
+++ components/Advantage.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Advantage = () => {
+  return (
+    <div className="my-16 md:px-8 md:my-0 ">
+      <h2 className="font-serif text-5xl my-16 md:my-0">Sustainable</h2>
+      <p className="max-w-2xl md:py-8">
+        You can use your own images to illustrate each statement and you can
+        replace the circle shape behind the image with another shape/image or
+        you just remove it.
+      </p>
+    </div>
+  );
+};
+
+export default Advantage;
